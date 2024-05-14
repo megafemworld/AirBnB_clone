4fcaeafe56fb71170d1541cc93f3fb7e08c9634a
Femi Mehalayese <megafemgroup@gmail.com>
2024-05-14 23:19:11 +0100

    file storage

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,18 +1,30 @@
-#!/usr/bin/python3
+#!/usr/bin/python3i
+import json
 """serializes instances to a JSON file and deserializes JSON file to instances"""
 
 class FileStorage:
-    __file_path
-    __objects
-    BaseModel
+    __file_path = "file.json"
+    __objects = {}
 
+    @property
     def all(self):
-        """returns the dictionary"""
+        """returns the dictionary __objects"""
+        return self.__objects
 
-    def new(self, obj):
+    @objects.setter
+    def new(self. obj):
+        """sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
 
     def save(self):
+        """ serializes __objects to the JSON file"""
+        with open(self.__file_path, 'w') as f:
+            json.dump(self._object, f)
+
 
     def reload(self):
-
-    
+        """deserializes the JSON file to __objects"""
+        if self.__file_path:
+            with open(self.__file_path, 'r') as f:
+                self.__objects = json.load(self.__file_path)
