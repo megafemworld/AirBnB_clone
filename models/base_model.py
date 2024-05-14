fd8116333f79fec929b0144fafc816d52c1a3051
Femi Mehalayese <megafemgroup@gmail.com>
2024-05-14 12:43:36 +0100

    baselmode

--- models/base_model.py
+++ models/base_model.py
@@ -2,17 +2,27 @@
 from uuid import uuid4
 from datetime import datetime
 """Parent class for BaselModel"""
-class BaselModel:
+
+class BaseModel:
     """ All attributes & methods of BaselModel"""
-    def __init__(self, id, created_at, updated_at):
+    def __init__(self):
         """ initilization of instance of BaselModel class"""
-        self.id = str(uuuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """String representation of an object"""
-        return f"[{class_name}] ({self.id}) {self.__dict__}"
+        return f"[{type(self).__name__}] ({self.id}) {self.__dict__}"
 
     def save(self):
-        self.update_at = datetime()
+        """update instance attributes update_at"""
+        self.update_at = datetime.now()
+
+    def to_dict(self):
+        """return dictionary containing all keys/values"""
+        obj_dict = self.__dict__.copy()
+        obj_dict['__class__'] = type(self).__name__
+        obj_dict['created_at'] = self.created_at.isoformat()
+        obj_dict['updated_at'] = self.updated_at.isoformat()
+        return obj_dict
