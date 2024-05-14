afb06642a20cdac64b7b8896b58320dab5bf526e
Femi Mehalayese <megafemgroup@gmail.com>
2024-05-14 15:16:35 +0100

    pp

---
+++ models/engine/file_storage.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""serializes instances to a JSON file and deserializes JSON file to instances"""
+
+class FileStorage:
+    __file_path
+    __objects
+    BaseModel
+
+    def all(self):
+        """returns the dictionary"""
+
+    def new(self, obj):
+
+    def save(self):
+
+    def reload(self):
+
+    
